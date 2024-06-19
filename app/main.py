ff08ded4a837a38de5649c393239c3fd7c462e3d
sudoskys <coldlando@hotmail.com>
2024-06-19 10:10:21 +0800

    update

--- app/main.py
+++ app/main.py
@@ -4,47 +4,57 @@
 # @File    : note_github_bot_test.py
 # @Software: PyCharm
 
-import os
-
-from github_bot_api import Event, Webhook
-from github_bot_api import GithubApp
-from github_bot_api.flask import create_flask_app
+import requests
+from flask import Flask, request
+from github import Github, GithubIntegration
 
 from settings.server import ServerSetting
 
-app = GithubApp(
-    user_agent='my-bot/0.0.0',
-    app_id=ServerSetting.github_app_id,
-    private_key=ServerSetting.github_private_key.get_secret_value()
+app = Flask(__name__)
+
+git_integration = GithubIntegration(
+    ServerSetting.github_app_id,
+    ServerSetting.github_private_key.get_secret_value(),
 )
 
-webhook = Webhook(secret=None)
 
+@app.route("/", methods=['POST'])
+async def bot():
+    # Get the event payload
+    payload = request.json
+    print(request.headers)
+    print(payload)
+    # Check if the event is a GitHub PR creation event
+    if not all(k in payload.keys() for k in ['action', 'pull_request']) and \
+            payload['action'] == 'opened':
+        return "ok"
 
-@webhook.listen('issues')
-def on_pull_request(event: Event) -> bool:
-    print(event.payload)
-    client = app.installation_client(event.payload['installation']['id'])
-    repo = client.get_repo(event.payload['repository']['full_name'])
-    issue = repo.get_issue(number=event.payload['issue']['number'])
-    issue.create_comment('Hello World')
-    return True
+    owner = payload['repository']['owner']['login']
+    repo_name = payload['repository']['name']
 
-
-@webhook.listen('issue_comment')
-def on_issue_comment(event: Event) -> bool:
-    print(event.payload)
-    print(event.delivery_id)
-    client = app.installation_client(event.payload['installation']['id'])
-    repo = client.get_repo(event.payload['repository']['full_name'])
-    issue = repo.get_issue(number=event.payload['issue']['number'])
-    issue.edit(
-        body=f"Hello World\n\n{issue.body}"
-
+    # Get a git connection as our bot
+    # Here is where we are getting the permission to talk as our bot and not
+    # as a Python webservice
+    git_connection = Github(
+        login_or_token=git_integration.get_access_token(
+            git_integration.get_installation(owner, repo_name).id
+        ).token
     )
-    return True
+    repo = git_connection.get_repo(f"{owner}/{repo_name}")
+
+    issue = repo.get_issue(number=payload['pull_request']['number'])
+
+    # Call meme-api to get a random meme
+    response = requests.get(url='https://meme-api.herokuapp.com/gimme')
+    if response.status_code != 200:
+        return 'ok'
+
+    # Get the best resolution meme
+    meme_url = response.json()['preview'][-1]
+    # Create a comment with the random meme
+    issue.create_comment(f"![Alt Text]({meme_url})")
+    return "ok"
 
 
-os.environ['FLASK_ENV'] = 'development'
-flask_app = create_flask_app(__name__, webhook)
-flask_app.run(host=ServerSetting.host, port=ServerSetting.port)
+if __name__ == "__main__":
+    app.run(debug=True, port=ServerSetting.port, host=ServerSetting.host)
