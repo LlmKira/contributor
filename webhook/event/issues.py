14231f2ccb90f8d5494ce6e04dd4d4d3dcf645fa
sudoskys <coldlando@hotmail.com>
2024-06-24 18:01:01 +0800

    🛠️ feat: add new attributes to Issue class
    - Added active_lock_reason and assignee attributes
    - Included author_association, body, and labels attributes

--- webhook/event/issues.py
+++ webhook/event/issues.py
@@ -19,16 +19,22 @@ class IssueUser(BaseUser):
 
 
 class Issue(BaseIssue):
-    active_lock_reason: str | None
+    active_lock_reason: str | None | Literal["off-topic", "too heated", "resolved", "spam"]
+    """
+    The reason the conversation was locked.
+    """
+    assignee: Optional[dict] = None
+    """State of the issue; either 'open' or 'closed'"""
     assignees: list | None
-    author_association: Union[str, Literal[
-        "COLLABORATOR", "CONTRIBUTOR", "FIRST_TIMER", "FIRST_TIME_CONTRIBUTOR", "MANNEQUIN", "MEMBER", "NONE", "OWNER"]
+    """The users that were assigned to the issue."""
+    author_association: str | Literal[
+        "COLLABORATOR", "CONTRIBUTOR", "FIRST_TIMER", "FIRST_TIME_CONTRIBUTOR", "MANNEQUIN", "MEMBER", "NONE", "OWNER"
     ]
     """
     How the author is associated with the repository.
-    Can be one of: COLLABORATOR, CONTRIBUTOR, FIRST_TIMER, FIRST_TIME_CONTRIBUTOR, MANNEQUIN, MEMBER, NONE, OWNER
     """
-    body: Optional[str]
+    body: str | None
+    """The body of the issue."""
     closed_at: str | None
     comments: int
     comments_url: str
@@ -37,24 +43,23 @@ class Issue(BaseIssue):
     events_url: str
     html_url: str
     id: int
+    labels: Optional[list] = None
     labels_url: str
+    locked: Optional[bool] = None
+    milestone: Optional[dict] = None
     node_id: str
     number: int
+    performed_via_github_app: Optional[dict] = None
+    pull_request: Optional[dict] = None
     reactions: dict
     repository_url: str
+    state: Optional[Union[str, Literal["open", "closed"]]] = None
+    state_reason: Optional[str] = None
+    timeline_url: Optional[str] = None
     title: str
     updated_at: str
     url: str
     user: dict
-    milestone: Optional[dict] = None
-    locked: Optional[bool] = None
-    assignee: Optional[dict] = None
-    labels: Optional[list] = None
-    performed_via_github_app: Optional[dict] = None
-    state: Optional[Union[str, Literal["open", "closed"]]] = None
-    """State of the issue; either 'open' or 'closed'"""
-    state_reason: Optional[str] = None
-    timeline_url: Optional[str] = None
 
 
 class OpenedIssueOpenEvent(BaseEvent):
@@ -71,7 +76,33 @@ class OpenedIssueOpenEvent(BaseEvent):
         """
         Get the issue object.
         :param integration: GithubIntegration
-        :param number: int
+        :return: Issue
+        """
+        return self.repository.get_issue(integration, issue_number=self.issue.number)
+
+    def get_repo(self, integration: GithubIntegration):
+        """
+        Get the repository object.
+        :param integration: GithubIntegration
+        :return:
+        """
+        return self.repository.get_repo(integration=integration)
+
+
+class ClosedIssueOpenEvent(BaseEvent):
+    action: Literal["closed"]
+    issue: Issue
+    repository: Repository
+    sender: Sender
+    organization: Optional[Organization] = None
+    installation: Optional[Installation] = None
+
+    model_config = ConfigDict(extra="allow")
+
+    def get_issue(self, integration: GithubIntegration):
+        """
+        Get the issue object.
+        :param integration: GithubIntegration
         :return: Issue
         """
         return self.repository.get_issue(integration, issue_number=self.issue.number)
