d636ee359909b6a48761d4bbdb0613ab7e4c4fa8
sudoskys <coldlando@hotmail.com>
2024-07-05 08:43:23 +0800

    :bug: fix(shared/schema): fix schema validation for cardSchema

--- shared/schema.ts
+++ shared/schema.ts
@@ -8,7 +8,7 @@ export enum Platform {
 
 // 自定义校验函数，用于检查 repoUrl 是否是一个有效的 GitHub 仓库地址
 const githubRepoUrlSchema = z
-    .string()
+    .string().max(100)
     .refine(
         (value) => {
             const githubRepoUrlPattern = /https?:\/\/github\.com\/[\w-]+\/[\w-]+/;
@@ -21,9 +21,9 @@ const githubRepoUrlSchema = z
 export const cardSchema = z.object({
     cardId: z.string().uuid().default(uuidv4),
     openaiEndpoint: z.string().url('Must be a valid URL.'),
-    apiModel: z.string(),
-    apiKey: z.string(),
-    userId: z.string(),
+    apiModel: z.string().max(100),
+    apiKey: z.string().max(100),
+    userId: z.string().max(100),
     repoUrl: githubRepoUrlSchema,
     disabled: z.boolean().default(false),
 });
@@ -32,10 +32,10 @@ export type CardT = z.infer<typeof cardSchema>;
 // User Schema
 export const userSchema = z.object({
     uid: z.string(),
-    name: z.string(),
+    name: z.string().max(100),
     login: z.string(),
     accessToken: z.string(),
-    avatarUrl: z.string().optional(),
+    avatarUrl: z.string().optional().default('https://avatars.githubusercontent.com/in/907205'),
     sourcePlatform: z.nativeEnum(Platform).optional(),
 });
 
